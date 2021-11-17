36120845ec8420be88410dc368757f3230961c41
ryuzaki979 <67687048+ryuzaki979@users.noreply.github.com>
2021-11-18 06:37:51 +0900

    removed contact section from now

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {HashRouter as Router,Route,Routes} from 'react-router-dom'
 import Navbar from "./component/Navbar";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
+
 function App() {
   return (
     <Router>
@@ -12,7 +12,6 @@ function App() {
       <Routes>
         <Route exact path = "/" element= {<Home/>} />
         <Route exact path = "/projects" element= {<Projects/>} />
-        <Route exact path = "/contact" element= {<Contact/>} />
       </Routes>
     </Router>
   );
--- src/pages/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ContactForm from "../component/ContactForm";
-const Contact = ()=>{
-    return <>
-        <ContactForm/>
-    </>
-}
-
-export default Contact;
